c6679d8acd6c00350066e2999e95f7a609549fe6
Arthur C N Ribeiro <105601110+arthurcnribeiro@users.noreply.github.com>
2024-08-15 10:29:17 -0500

    Manipulação de Strings

---
+++ estruturas_condicionais/manipulacao_de_string.py
@@ -0,0 +1,44 @@
+"""curso = " Python "
+print(curso.upper())
+print(curso.lower())
+print(curso.title())
+print(curso.strip())
+print(curso.lstrip())
+print(curso.rstrip())
+print(curso.center(30, "#"))
+print(" . ".join(curso))"""
+#"****************************************************
+"""PI = 3.14159
+print(f"Valor de PI: {PI}")
+print(f"Valor de PI: {PI:.2f}")# 2 casas decimais após a virgula"""
+#*****************************************************
+"""nome = "Arthur Camero Nascimento Ribeiro"
+print(nome[3])
+print(nome[:13])
+print(nome[14:])
+print(nome[14:24])
+print(nome[10:20:2])
+print(nome[:])
+print(nome[::-1])
+print(nome[-1])"""
+#*****************************************************
+nome = "Arthur"
+msg = (f"""Olá meu
+       nome é {nome} e eu estou
+        aprendendo Python""")
+print(msg)
+print(f"""
+    ********* MENU **********
+        
+    1 - SACAR
+    2 - DEPOSITAR
+    3 - {nome}
+    -------------------------
+""")
+
+
+
+
+
+
+
