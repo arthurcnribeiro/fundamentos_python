8cfc76ebc60ea22e090548e884ebd697f12ac3ca
Arthur C N Ribeiro <105601110+arthurcnribeiro@users.noreply.github.com>
2024-08-14 19:50:50 -0500

    Ajustando a Readme

---
+++ estruturas_condicionais/estruturas_de_repeticao.py
@@ -0,0 +1,29 @@
+"""texto = input("Digite um texto: ")
+VOGAIS = "AEIOU"
+
+for letra in texto:
+    if letra.upper() in VOGAIS:
+        print(letra, end="")
+print()"""
+#print(list(range(4)))
+"""for numero in range(0, 51, 5):# start, range -1, de 5 em 5
+    print(numero, end=" ")"""
+"""opcao = -1
+while opcao != 0:
+    opcao = int(input("[1] Sacar \n[2] Extrato \n[0] Sair \n>>>: ))
+
+    if opcao == 1:
+        print("Sacando...")
+    if opcao == 2:
+        print("Exibindo Extrado...")"""
+
+"""while True:
+    opcao = int(input("Digite um número: "))
+
+    if opcao == 10:
+        break
+    print(opcao)"""
+for numero in range(101):
+    if numero % 2 == 1:
+        continue
+    print(numero, end=" ")
