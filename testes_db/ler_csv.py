aa78c34590785c4a12bd44d72496e082ea19a77e
Arthur C N Ribeiro <105601110+arthurcnribeiro@users.noreply.github.com>
2024-08-17 20:56:00 -0500

    filtrar, salvar e excluir dados no csv

--- testes_db/ler_csv.py
+++ testes_db/ler_csv.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Especificando o caminho para o arquivo CSV
-tabela_2 = "db/tabela_2.csv"# Lendo o arquivo CSV
-dados = pd.read_csv(tabela_2) #ler toda a tabela
+tabela_1 = "db/tabela_1.csv" # Lendo o arquivo CSV
+dados = pd.read_csv(tabela_1, delimiter=";") #ler toda a tabela
 #dados = pd.read_csv(tabela_1, usecols=['nome_cliente', 'modelo'])#Ler apenas as colunas que você der o nome
 
 print(dados)
