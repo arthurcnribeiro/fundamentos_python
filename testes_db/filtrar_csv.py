7c9bd43b17a5400255535f2a08d4123b13fee5e4
Arthur C N Ribeiro <105601110+arthurcnribeiro@users.noreply.github.com>
2024-08-21 09:08:09 -0500

    filtrando_e_salvando

--- testes_db/filtrar_csv.py
+++ testes_db/filtrar_csv.py
@@ -1,27 +1,15 @@
-#import pandas as pd
-
-# Especificando o caminho para o arquivo CSV
-"""tabela_1 = "db/tabela_1.csv" # Lendo o arquivo CSV
-dados = pd.read_csv(tabela_1, delimiter=";") #ler toda a tabela
-#dados = pd.read_csv(tabela_1, usecols=['nome_cliente', 'modelo'])#Ler apenas as colunas que você der o nome
-#print(dados)
-#***********************************************************
-#print()
-colunas_especificas = dados[['nome_cliente', 'fabricante']]
-print(colunas_especificas)"""
-#*************************************************************
-"""linhas_filtradas = dados[dados['quilometragem'] <= 3000]
-print(linhas_filtradas)"""
-#print()
-
 import pandas as pd
+
 # Ler o arquivo CSV
 df = pd.read_csv('db/tabela_1.csv', delimiter=";")
 
 # Passar a coluna 'nome' para maiúscula
 df['modelo'] = df['modelo'].str.upper()
+df['nome_cliente'] = df['nome_cliente'].str.lower()
 
 # Filtrar onde a coluna 'nome' é igual a 'JOÃO'
 filtro = df[df['modelo'].str.contains("CIVIC")]
-
 print(filtro)
+
+# Salvar os dados filtrados em um arquivo CSV já existente
+filtro.to_csv('db/tabela_3.csv', mode='w', index=False, header=True)
