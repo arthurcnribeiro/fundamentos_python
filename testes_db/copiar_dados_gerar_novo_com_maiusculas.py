1942202d2f085e6676d62fedfc123a93270ca627
Arthur C N Ribeiro <105601110+arthurcnribeiro@users.noreply.github.com>
2024-08-21 14:24:14 -0500

    copiar dados e gerar um novo arquivo com maiúsculas

--- testes_db/copiar_dados_gerar_novo_com_maiusculas.py
+++ testes_db/copiar_dados_gerar_novo_com_maiusculas.py
@@ -1,22 +1,26 @@
-def converter_maiusculas(arquivo_entrada, arquivo_saida):
+def juntar_arquivos(arquivo_1, arquivo_2, arquivo_3):
 
     try:
-        with open(arquivo_entrada, 'r') as entrada:
-            conteudo = entrada.read()  # Lê o conteúdo do arquivo de entrada
+        with open(arquivo_1, 'r') as dados_1:
+            conteudo_1 = dados_1.read()  # Lê o conteúdo do arquivo de entrada
 
-            # Substitui por maiúscula
-            conteudo_convertido = conteudo.upper()
+        with open(arquivo_2, 'r') as dados_2:
+            conteudo_2 = dados_2.read()
 
-        with open(arquivo_saida, 'w') as saida:
-            saida.write(conteudo_convertido)  # Escreve o conteúdo modificado no arquivo de saída
+        with open(arquivo_3, 'w') as saida:
+            saida.write(conteudo_1)  # Escreve o conteúdo modificado no arquivo de saída
 
-        print(f"O arquivo '{arquivo_saida}' foi criado com letras maiúsculas")
+        with open(arquivo_3, 'a') as saida:
+            saida.write(conteudo_2)
+
+        print(f"O arquivo 3 foi criado com sucesso!")
 
     except FileNotFoundError:
         print("Arquivo de entrada não encontrado. Por favor, verifique o nome e tente novamente.")
 
 
 # Solicita o nome do arquivo de entrada e o nome do arquivo de saída ao usuário
-arquivo_entrada = input("Digite o nome do arquivo de entrada (com a extensão): ")
-arquivo_saida = input("Digite o nome do arquivo de saída (com a extensão): ")
-converter_maiusculas(arquivo_entrada, arquivo_saida)
+arquivo_1 = input("Digite o nome do arquivo 1 (com a extensão): ")
+arquivo_2 = input("Digite o nome do arquivo 2 (com a extensão): ")
+arquivo_3 = input("Digite o nome do arquivo 3 (com a extensão): ")
+juntar_arquivos(arquivo_1, arquivo_2, arquivo_3)
